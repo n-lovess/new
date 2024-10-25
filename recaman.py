336c5da34bd053a38fba001c4ff00364d27c6ef1
n-lovess <fwlm5667@leeds.ac.uk>
2024-10-25 14:33:33 +0000

    save

--- recaman.py
+++ recaman.py
@@ -1,14 +1,16 @@
-L = int(input("Length of sequence: ")) 
-sequence = [0] 
+L = int(input())  # Only take the integer input for length
 
-for k in range(1, L): 
-    prev_value = sequence[k - 1]
-    next_value = prev_value - k
-    
-    if next_value > 0 and next_value not in sequence: 
+sequence = [0]  # Start the sequence with 0
+
+for k in range(1, L):  # Loop starting from 1 up to the length
+    prev_value = sequence[k-1]  # Previous value in the sequence
+    next_value = prev_value - k  # Try subtracting k
+
+    # Check if next_value is positive and not in the sequence
+    if next_value > 0 and next_value not in sequence:
         sequence.append(next_value)
     else:
-        sequence.append(prev_value + k)
+        sequence.append(prev_value + k)  # Otherwise, add k to prev_value
 
-
+# Print the sequence as a single line of space-separated integers
 print(" ".join(map(str, sequence)))
