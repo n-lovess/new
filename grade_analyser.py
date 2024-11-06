480c06b0f90f268d81a960e63c14a7e389006e7a
n-lovess <fwlm5667@leeds.ac.uk>
2024-11-06 15:52:06 +0000

    save

---
+++ grade_analyser.py
@@ -0,0 +1,9 @@
+grades = [75, 88, 92, 67, 85]
+
+average_grade = sum(grades)
+highest_grade = max(grades)
+lowest_grade = min(grades)
+
+print("Average Grade:", average_grade)
+print("Highest Grade:", highest_grade)
+print("Lowest Grade:", lowest_grade)
