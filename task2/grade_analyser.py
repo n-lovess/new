b49c4f84af475920868ce6abaf42496ca23d2e3d
n-lovess <fwlm5667@leeds.ac.uk>
2024-11-06 16:12:28 +0000

    save

--- task2/grade_analyser.py
+++ task2/grade_analyser.py
@@ -1,10 +1,16 @@
+import os
+
+input_filename = "grades.csv" 
+
+output_filename = os.path.splitext(input_filename)[0] + "_out.csv"
+
 grades = [75, 88, 92, 67, 85]
 
 average_grade = round(sum(grades) / len(grades), 2)
 highest_grade = max(grades)
 lowest_grade = min(grades)
 
-with open("grades_output.txt", "w") as file:
+with open(output_filename, "w") as file:
     file.write(f"Average Grade: {average_grade:.2f}\n")
     file.write(f"Highest Grade: {highest_grade}\n")
     file.write(f"Lowest Grade: {lowest_grade}\n")
