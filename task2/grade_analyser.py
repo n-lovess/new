eff8503db20970eb026a93b6b89ee2c84bcbc241
n-lovess <fwlm5667@leeds.ac.uk>
2024-11-12 15:32:49 +0000

    saving

--- task2/grade_analyser.py
+++ task2/grade_analyser.py
@@ -1,5 +1,6 @@
 import csv
 
+
 def classify_grade(average):
     if average >= 70:
         return '1'
@@ -12,27 +13,36 @@ def classify_grade(average):
     else:
         return 'F'
 
+
 def analyze_grades(filename):
     output_filename = f"{filename}_out.csv"
-
+    
     with open(filename, mode='r') as infile, open(output_filename, mode='w', newline='') as outfile:
         reader = csv.reader(infile)
         writer = csv.writer(outfile)
+        
+
+        first_row = next(reader)
+        if not first_row[0].isdigit():  
+            pass  
+        else:
+            infile.seek(0)  
+
 
         for row in reader:
             student_id = row[0]
-            grades = [int(grade) for grade in row[1:] if grade]  # Ignore empty cells
+            grades = [int(grade) for grade in row[1:] if grade.isdigit()]
             
-            if grades:  
+            if grades:
                 average_grade = sum(grades) / len(grades)
             else:
-                average_grade = 0 
+                average_grade = 0
             
             classification = classify_grade(average_grade)
-
             writer.writerow([student_id, f"{average_grade:.2f}", classification])
     
     print(f"Data written to {output_filename}")
+
 
 filename = input("Enter the filename of the student file: ")
 analyze_grades(filename)
