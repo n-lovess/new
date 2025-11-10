a26c58ae112a33b55859d56c302f5f65a499cf7a
n-lovess <fwlm5667@leeds.ac.uk>
2025-11-10 12:24:47 +0000

    save

--- lu_factorisation.py
+++ lu_factorisation.py
@@ -1,24 +1,45 @@
-import numpy as np
-
 def lu_factorisation(A):
     """
-    Compute the LU factorisation of a square matrix A such that A = L U
-    """
-    A = A.astype(float)
-    n = A.shape[0]
-    L = np.zeros_like(A)
-    U = np.zeros_like(A)
+    Compute the LU factorisation of a square matrix A.
 
+    The function decomposes a square matrix ``A`` into the product of a lower
+    triangular matrix ``L`` and an upper triangular matrix ``U`` such that:
+
+    .. math::
+        A = L U
+
+    where ``L`` has unit diagonal elements and ``U`` is upper triangular.
+
+    Parameters
+    ----------
+    A : numpy.ndarray
+        A 2D NumPy array of shape ``(n, n)`` representing the square matrix to
+        factorise.
+
+    Returns
+    -------
+    L : numpy.ndarray
+        A lower triangular matrix with shape ``(n, n)`` and unit diagonal.
+    U : numpy.ndarray
+        An upper triangular matrix with shape ``(n, n)``.
+    """
+    n, m = A.shape
+    if n != m:
+        raise ValueError(f"Matrix A is not square {A.shape=}")
+
+    # construct arrays of zeros
+    L, U = np.zeros_like(A), np.zeros_like(A)
+
+    # ...
     for i in range(n):
-        # upper triangle
         for j in range(i, n):
-            sum_val = sum(L[i, k] * U[k, j] for k in range(i))
-            U[i, j] = A[i, j] - sum_val
+            sum_val = sum(L[i ,k] * U[k, j] for k in range(i))
+            U[i, j] = A[i,j] - sum_val
 
         L[i, i] = 1.0
 
-        for j in range(i + 1, n):
+        for j  in range(i + 1, n):
             sum_val = sum(L[j, k] * U[k, i] for k in range(i))
-            L[j, i] = (A[j, i] - sum_val) / U[i, i]
+            L[j, i] = (A[j, i] - sum_val)  / U[i ,i]
 
     return L, U
